33fbc863011c4ce279400ed5e3945126056e9992
Akshaybagai52 <akshaybagai52@gmail.com>
2024-10-16 09:59:11 +0530

    feat: added header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { HeadersAdapter } from "next/dist/server/web/spec-extension/adapters/headers";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Schedulrr",
@@ -16,9 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
           {children}
         </main>
+        <footer className="bg-blue-100 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made with 💖 by Akshay</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
